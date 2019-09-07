f7a2f81d5e0534a9860b13df2dffd1eb45cd06b7
Ahmed Saeed <A7med19962014@gmail.com>
2019-09-07 14:39:49 +0200

    any thing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import Home from "./pages/Home";
 import Rooms from "./pages/Rooms";
@@ -13,12 +13,14 @@ function App() {
   return (
     <>
       <Navbar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path="/rooms" component={Rooms} />
-        <Route exact path="/rooms/:id" component={SingleRoom} />
-        <Route component={Error} />
-      </Switch>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/rooms" component={Rooms} />
+          <Route exact path="/rooms/:id" component={SingleRoom} />
+          <Route component={Error} />
+        </Switch>
+      </Router>
     </>
   );
 }
